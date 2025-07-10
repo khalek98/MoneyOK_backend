1809de4580a2cf8f74ed3f285a3b9a6dd076871c
Khalek <xalek.islam@gmail.com>
2025-07-10 15:11:51 +0300

    Update .gitignore and remove build and yarn files
    Added .yarn and dist directories to .gitignore. Removed compiled files from dist/ and Yarn release and state files to clean up the repository.

--- dist/validations/wallet.validate.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateUpdateWallet = exports.validateCreateWallet = void 0;
-const express_validator_1 = require("express-validator");
-exports.validateCreateWallet = [(0, express_validator_1.body)("name", "Wallet Name is required").isString().notEmpty()];
-exports.validateUpdateWallet = [
-    (0, express_validator_1.body)("name").optional().isString().withMessage("Name type must be string"),
-    (0, express_validator_1.body)("balance").optional().isNumeric().withMessage("Balance must be a number"),
-];
